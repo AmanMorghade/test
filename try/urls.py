19e8b7857914291fd6375f2b78dbf750c53e2f0c
AmanMorghade <amanmorghade1@gmail.com>
2025-07-02 14:14:27 +0530

    form

--- try/urls.py
+++ try/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',tryss,name="tryss")
+    # path('',tryss,name="tryss"),
+    path('', person_create, name='person_create'),
+    # path('success/', success_view, name='success'),
+    path('list/', person_list, name='person_list'),
 ]
--- try/views.py
+++ try/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
+from django.shortcuts import render, redirect
+from .forms import PersonForm
 # Create your views here.
+
+
 def tryss(request):
     return render(request,"base.html")
+
+
+
+
+def person_create(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('person_list')
+    else:
+        form = PersonForm()
+    return render(request, 'person_form.html', {'form': form})
+
+
+def person_list(request):
+    people = Person.objects.all()
+    return render(request, 'person_list.html', {'people': people})
